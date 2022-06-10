bcd1188c2f3ba8ab988a0dd1c72fb6496b5d3163
Rena <kanagurena@gmail.com>
2022-06-10 17:17:46 +0530

    title changed

--- GitProject/ViewController.swift
+++ GitProject/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        title = "hai"
+        title = "haihello"
         view.backgroundColor = .systemRed
         
     }
